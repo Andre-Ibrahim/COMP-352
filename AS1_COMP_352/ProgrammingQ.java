3f2e099caadb1ad8546cb9049c51554483840dfa
Andreibra1 <Andreibr1@users.noreply.github.com>
2020-05-11 21:37:15 -0400

    fixed version 1 pseudo code left and version 2

--- AS1_COMP_352/ProgrammingQ.java
+++ AS1_COMP_352/ProgrammingQ.java
@@ -1,32 +1,33 @@
-import java.util.ArrayList;
-
 public class ProgrammingQ{
     public static void main(String[] args) {
 
-        permu("bca", "hhhlajkjgabckkkkcbakkdfjknbbca", 0);// test case from the pdf
+        permu("abc", "hhhlajkjgabckkkkcbakkdfjknbbca", 0, 2);// test case from the pdf
         
     }
-    //version 1: recursive case
-    public static int permu(String shortStr, String longStr, int index){
-    
-        String nString = new String(longStr);                                             // building a new string that will be used as an argument 
-        int len = shortStr.length();                                                     // length of the short string which would be helpful for the base case
-        if(len > longStr.length()){                                                      // base case 1 is if the long string is smaller than  the length
+
+    public static void permu(String shortStr, String longStr, int start, int end){
+        if (start == end){// base case if the start is equal to the end we use that shortString to do our search
             System.out.println(shortStr);
-            return -1;// returning -1 for no matches
+
+            for(int i = 0; i < longStr.length() - shortStr.length() + 1; i++){// search with O(n)
+                if (longStr.substring(i, i + shortStr.length()).equals(shortStr))
+                    System.out.println("found one match: " + shortStr + " is in " + longStr + " at location " + i);
+
+            } 
         }
-        else if(shortStr.equals(longStr.substring(0, len))){ // if there is  a match return the index
-            System.out.println(shortStr);
-            System.out.println("found one match: " + shortStr + "is in " + longStr + " at location " + index);
-            return index;   
-        }
-        else{
-            index++;    // incrementing index value if no match
-            nString = nString.substring(1);
-            permu(shortStr, nString, index);
-        }
-        return -1;//should never occure but makes compiler happy
-        
-    }//rip
-    
+        else { 
+            for (int i = start; i <= end; i++) { // swaping values and calling recursivly calling the function to keep swaping until base case is attained
+                shortStr = swap(shortStr, start, i); 
+                permu(shortStr, longStr, start + 1, end); // calling function on new string with swapped values
+            } 
+        } 
+    }
+    public static String swap(String str, int s, int e){ // since the swap is done more then once a helper method is ideal
+        char temp; 
+        char[] charArray = str.toCharArray(); 
+        temp = charArray[s]; 
+        charArray[s] = charArray[e]; 
+        charArray[e] = temp; 
+        return String.valueOf(charArray); 
+    }
 }
